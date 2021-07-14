7691ea4933fdf3ad10485d6335d0a72b5c3abd81
Предраг Николић / Predrag Nikolic <idmpepe@gmail.com>
2021-07-14 20:43:07 +0200

    Improve is_allowed_to_start (#14)
    * remove is_allowed_to_start
    
    as it doesn't allow the server to initialize if the tailwind.config.js is in a sub directory
    
    * Revert "remove is_allowed_to_start"
    
    This reverts commit 4a64d2fc8cec6d4eb3672b679bd103831143cc00.
    
    * add find_file_in_workspace
    
    It recuresvly finds the file pattern in the workspace and returns the path to that file if it exist or None if it doesn't exist.
    
    * add new line at the end
    
    * use word pattern instead of regex
    
    i just want to be consistent, I don't have a preference between those two words
    
    * fix: use fnmatch instead of re for folder_excldue_patterns
    
    While fnmatch is still not perfect in terms of supporting sophisitcated glob patterns. It supports `*` and `?`.
    
    Time will tell(or Rafal or Raoul) if we need something more bullet proof.
    
    * escape dots, and add ^ and $ in regex
    
    * use set
    
    * fix typo
    
    * use re.match instead of re.search
    
    Co-authored-by: Predrag Nikolic <mail@predrag.pro>

--- plugin.py
+++ plugin.py
@@ -1,8 +1,10 @@
+import fnmatch
 import os
+import re
 import sublime
 from LSP.plugin import ClientConfig
 from LSP.plugin import WorkspaceFolder
-from LSP.plugin.core.typing import List, Optional
+from LSP.plugin.core.typing import List, Optional, Set
 from lsp_utils import NpmClientHandler
 
 
@@ -29,9 +31,25 @@ class LspTailwindcssPlugin(NpmClientHandler):
     ) -> Optional[str]:
         if not workspace_folders:
             return "Requires a folder to start."
-        path = workspace_folders[0].path
-        tailwind_config_file_path = os.path.join(path, 'tailwind.config.js')
-        if not os.path.exists(tailwind_config_file_path):
-            return "No tailwind.config.js present in {}".format(path)
-        return None
 
+        # Config pattern is found here:
+        # https://github.com/tailwindlabs/tailwindcss-intellisense/blob/766a5d533dcb68640ce6b3270488f6701dd1173d/packages/vscode-tailwindcss/src/extension.ts#L40
+        config_file_pattern = r'^(tailwind|tailwind\.config)\.(js|cjs)$'
+        folder_exclude_patterns = set(sublime.load_settings('Preferences.sublime-settings').get("folder_exclude_patterns", [])) # type: Set[str]
+        folder_exclude_patterns.add('node_modules') # definitely exclude node_modules
+        config_file = find_file_in_workspace(config_file_pattern, workspace_folders[0].path, folder_exclude_patterns)
+        if config_file:
+            return None # config found, return None to start the session
+        return "No tailwind configuration file present in the workspace folder."
+
+
+def find_file_in_workspace(file_pattern: str, root_folder: str, folder_exclude_patterns: Set[str] = None) -> Optional[str]:
+    for path, directories, files in os.walk(root_folder):
+        for folder_exclude_pattern in folder_exclude_patterns or []:
+            # skip ignored folders
+            directories[:] = [d for d in directories if not fnmatch.fnmatch(d, folder_exclude_pattern)]
+
+        for file in files:
+            if re.match(file_pattern, file):
+                return os.path.join(path, file)
+    return None
