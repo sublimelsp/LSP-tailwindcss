00b192d3ef0a8027e18757837a26aaa7b3fc6567
Предраг Николић <idmpepe@gmail.com>
2021-10-11 19:16:57 +0200

    show message in the status bar if tailwindcss dependency is missing

--- download-language-server.sh
+++ download-language-server.sh
@@ -25,6 +25,7 @@ unzip -a "$tarball" -d ./language-server-temp
 # ./language-server/package.json is required for lsp_utils to work. Reuse package.json from the extension folder.
 cp ./language-server-temp/extension/package.json ./language-server
 cp -R ./language-server-temp/extension/dist/server ./language-server
+cp ./resolve_module.js ./language-server # move script to language server dir
 
 # ./language-server/package-lock.json is required. Without it an error will appear when when starting the plugin.
 cd language-server && npm i --production && cd ..
--- plugin.py
+++ plugin.py
@@ -1,12 +1,12 @@
-import fnmatch
-import os
-import re
-import sublime
 from LSP.plugin import ClientConfig
 from LSP.plugin import WorkspaceFolder
 from LSP.plugin.core.typing import List, Optional, Set
 from lsp_utils import NpmClientHandler
-
+import fnmatch
+import os
+import re
+import sublime
+import subprocess
 
 def plugin_loaded():
     LspTailwindcssPlugin.setup()
@@ -36,15 +36,28 @@ class LspTailwindcssPlugin(NpmClientHandler):
         # Config pattern is found here:
         # https://github.com/tailwindlabs/tailwindcss-intellisense/blob/766a5d533dcb68640ce6b3270488f6701dd1173d/packages/vscode-tailwindcss/src/extension.ts#L40
         config_file_pattern = r'^(tailwind|tailwind\.config)\.(js|cjs)$'
-        folder_exclude_patterns = set(sublime.load_settings('Preferences.sublime-settings').get("folder_exclude_patterns", [])) # type: Set[str]
-        folder_exclude_patterns.add('node_modules') # definitely exclude node_modules
+        folder_exclude_patterns = set(
+            sublime.load_settings('Preferences.sublime-settings').get("folder_exclude_patterns", [])
+        )  # type: Set[str]
+        folder_exclude_patterns.add('node_modules')  # definitely exclude node_modules
         config_file = find_file_in_workspace(config_file_pattern, workspace_folders[0].path, folder_exclude_patterns)
-        if config_file:
-            return None # config found, return None to start the session
-        return "No tailwind configuration file present in the workspace folder."
+        if not config_file:
+            return "No tailwind configuration file present in the workspace folder."
+        if not LspTailwindcssPlugin.is_tailwind_installed(workspace_folders[0].path):
+            return "`tailwindcss` dependency is not installed. Run `npm install` in the workspace to enable LSP-tailwindcss."
+        return None  # config found, return None to start the session
+
+    @classmethod
+    def is_tailwind_installed(cls, root_folder: str) -> bool:
+        server_directory_path = cls._server_directory_path()
+        resolve_module_script = os.path.join(server_directory_path, 'resolve_module.js')
+        command = [cls._node_bin(), resolve_module_script, root_folder, 'tailwindcss']
+        tailwindcss_path = subprocess.check_output(command, universal_newlines=True)
+        return bool(tailwindcss_path)
 
 
-def find_file_in_workspace(file_pattern: str, root_folder: str, folder_exclude_patterns: Set[str] = None) -> Optional[str]:
+def find_file_in_workspace(file_pattern: str, root_folder: str,
+                           folder_exclude_patterns: Set[str] = None) -> Optional[str]:
     for path, directories, files in os.walk(root_folder):
         for folder_exclude_pattern in folder_exclude_patterns or []:
             # skip ignored folders
