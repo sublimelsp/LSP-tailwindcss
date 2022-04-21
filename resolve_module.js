17b2815d17b7a55bef06a2089908d1b31b990f2a
Предраг Николић <idmpepe@gmail.com>
2022-04-21 02:05:29 -0700

    Switch to npm package (#38)
    * switch to npm package
    
    Instead of relying on the script to download the server.
    Additional pro is that dependabot will create PR when it detects new versions on npm
    
    * remove the other resolve module script
    
    * add dependabot config
    
    * still point to a shell script...

--- resolve_module.js
+++
@@ -1,7 +0,0 @@
-try {
-	const workspaceDir = process.argv[2]
-	const module = process.argv[3]
-	process.stdout.write(require.resolve(module, { paths: [workspaceDir] }));
-} catch {
-	// ignore error if module not found
-}
