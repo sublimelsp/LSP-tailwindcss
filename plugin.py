1b986717c1928eb79476e8d716fabc302fde1642
Предраг Николић <idmpepe@gmail.com>
2024-10-12 00:46:18 +0200

    require node version 18.17.0
    ref: https://github.com/tailwindlabs/tailwindcss-intellisense/pull/978/files
    
    Someone had exactly node v18.0.0 and tailwindcss didn't work with that version, thus the version is set to v18.17.0

--- plugin.py
+++ plugin.py
@@ -14,3 +14,7 @@ class LspTailwindcssPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'language-server'
     server_binary_path = os.path.join(server_directory, 'node_modules', '@tailwindcss', 'language-server', 'bin', 'tailwindcss-language-server')
+
+    @classmethod
+    def required_node_version(cls) -> str:
+        return '>=18.17.0'
